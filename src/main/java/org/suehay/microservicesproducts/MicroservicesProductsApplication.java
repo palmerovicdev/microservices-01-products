a7a51efadd050cdf5c0e7c1e0da360cc7160bf89
palmerodev <victor.palmero@undostres.com.mx>
2023-12-11 00:42:37 -0500

    create microservices products

---
+++ src/main/java/org/suehay/microservicesproducts/MicroservicesProductsApplication.java
@@ -0,0 +1,13 @@
+package org.suehay.microservicesproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesProductsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesProductsApplication.class, args);
+    }
+
+}
