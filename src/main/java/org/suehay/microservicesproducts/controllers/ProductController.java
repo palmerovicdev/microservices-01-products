faa724daac791d6e2393c34ec1adc2441074f1e7
palmerodev <victor.palmero@undostres.com.mx>
2023-12-11 03:50:43 -0500

    implemented some crud request

--- src/main/java/org/suehay/microservicesproducts/controllers/ProductController.java
+++ src/main/java/org/suehay/microservicesproducts/controllers/ProductController.java
@@ -19,4 +19,29 @@ public class ProductController {
     public ResponseEntity<ProductResponse> create(@RequestBody ProductRequest product) {
         return ResponseEntity.ok(this.productService.create(product));
     }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<ProductResponse> update(@PathVariable Long id, @RequestBody ProductRequest product) {
+        return ResponseEntity.ok(this.productService.update(id, product));
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<ProductResponse> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(this.productService.findById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        this.productService.delete(id);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<?> findAll() {
+        return ResponseEntity.ok(this.productService.findAll());
+    }
+
 }
